22c914b65c038a72dc9dfbd48ce2240098fc44c9
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-07-18 14:51:16 +0800

    chore: delete

--- packages/trpc/src/server/server.ts
+++
@@ -1,9 +0,0 @@
-import { initTRPC } from "@trpc/server";
-import { z } from "zod";
-
-const t = initTRPC.create();
-const publicProcedure = t.procedure;
-
-const appRouter = t.router({});
-export type AppRouter = typeof appRouter;
-
